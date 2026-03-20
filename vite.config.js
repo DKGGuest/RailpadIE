c0abd171cdbe3d42ac0764e754ee2e2f8b46dbd0
ujjawalverma1 <clpujjwal@dkgrouplabs.com>
2026-03-20 17:12:22 +0530

    base url

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/Railpad-IE/',
+  base: '/',
   esbuild: {
     loader: "jsx",
     include: /src\/.*\.jsx?$/,
